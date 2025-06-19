7af3aa888bdfcf7811e81343ac616d801bb9d700
Roman Pena <roman.pena715@gmail.com>
2025-06-19 10:42:15 -0400

    added request to github api for starred python repos

---
+++ python_repos.py
@@ -0,0 +1,13 @@
+import requests
+
+# query github api for python repos with > 10000 stars
+url = "https://api.github.com/search/repositories"
+url += "?q=language:python+sort:stars+stars:>10000"
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
+print(f"Status code: {r.status_code}")
+
+response_dict = r.json()
+
+print(response_dict.keys())
